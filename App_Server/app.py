e89347a8c724075f3679e6d0a52dee64012f9b3e
yosammy01 <samueleverardorobles@gmail.com>
2026-03-22 08:50:06 -0400

    prep for AKS deployment
    1. adding AKS database login info
    2. adding yaml files for building and pushing images to ACR

--- App_Server/app.py
+++ App_Server/app.py
@@ -11,7 +11,14 @@ container_id = socket.gethostname()
 def get_db_connection():
     return mysql.connector.connect(
         # ------------------------------
-        # PRODUCTION SETTINGS (UNCOMMENT FOR DEPLOYMENT)
+        # DOCKER SETTINGS (UNCOMMENT FOR DOCKER DEPLOYMENT)
+        host='db', # This matches the service name in your docker-compose.yml
+        port=3306,
+        user='root',
+        password='root',
+
+        # ------------------------------
+        # DOCKER SETTINGS (UNCOMMENT FOR DOCKER DEPLOYMENT)
         # host='db', # This matches the service name in your docker-compose.yml
         # port=3306,
         # user='flask_user',
@@ -19,10 +26,10 @@ def get_db_connection():
 
         # ------------------------------
         # LOCAL DEVELOPMENT SETTINGS (UNCOMMENT FOR LOCAL TESTING)
-        host='localhost',
-        port=3307,
-        user='root',  
-        password='root',
+        # host='localhost',
+        # port=3307,
+        # user='root',  
+        # password='root',
         # ------------------------------
 
         database='gsu_catalog'
